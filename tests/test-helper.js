2967e30ce50e576633f5ff3a34fbd65a952403d5
Benjamin West <23172276+bgwest@users.noreply.github.com>
2022-11-02 13:02:20 -0700

    ember init

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'ember-advanced-tutorial/app';
+import config from 'ember-advanced-tutorial/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
