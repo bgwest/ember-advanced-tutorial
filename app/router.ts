1317f2c3f0ca6433723be35bdb9c21075f9e1f9d
Benjamin West <23172276+bgwest@users.noreply.github.com>
2022-11-03 11:13:19 -0700

    added my first set of links

--- app/router.ts
+++ app/router.ts
@@ -6,4 +6,10 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  // route = name or route, not the path
+  // { path } = actual uri. in fact, if uri = route name then a
+  //            path declaration isn't needed
+  this.route('about');
+  this.route('contact', { path: '/getting-in-touch' });
+});
